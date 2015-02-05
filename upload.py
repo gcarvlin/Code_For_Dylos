e3e24d74827a80d381c932f04668f2a480a9cb31
Brian High <bhigh@live.com>
2015-02-04 17:39:20 -0800

    Trying for more readability.

--- upload.py
+++ upload.py
@@ -1,8 +1,34 @@
 import subprocess
 import sys
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[3]+", \"tags\" : { \"Bin\": \"bin1\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[4]+", \"tags\" : { \"Bin\": \"bin2\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[5]+", \"tags\" : { \"Bin\": \"bin3\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[6]+", \"tags\" : { \"Bin\": \"bin4\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[7]+"."+sys.argv[8]+", \"tags\" : { \"Temp\": \"temp\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
-subprocess.call(["curl","https://replicant.deohs.washington.edu/api/v1/datapoints","-u","USERNAME:PASS","-d"," [  {    \"name\" : \""+sys.argv[1]+"\", \"timestamp\": "+sys.argv[2]+"000, \"value\" : "+sys.argv[9]+"."+sys.argv[10]+", \"tags\" : { \"RH\": \"rh\" }}]","--cacert","/etc/ssl/certs/cacert4.pem"])
+import json
+
+# Initialize variables
+url="https://replicant.deohs.washington.edu/api/v1/datapoints"
+cert="/etc/ssl/certs/cacert4.pem"
+auth="USERNAME:PASS"
+
+# Functions
+def prepdata(value, tags):
+    "This prepares a data structure to send in JSON format"
+    data = [
+               {
+                   "name" :      sys.argv[1], 
+                   "timestamp" : sys.argv[2] + "000", 
+                   "value":      value, 
+                   "tags" :      tags
+               }
+           ]
+    return data
+
+# Create data structures
+bin1 = prepdata(sys.argv[3], { "Bin" : "bin1" })
+bin2 = prepdata(sys.argv[4], { "Bin" : "bin2" })
+bin3 = prepdata(sys.argv[5], { "Bin" : "bin3" })
+bin4 = prepdata(sys.argv[6], { "Bin" : "bin4" })
+temp = prepdata(sys.argv[7] + "." + sys.argv[8], { "Temp" : "temp" })
+rh = prepdata(sys.argv[9] + "." + sys.argv[10], { "RH" : "rh" })
+
+# Send data in JSON format using curl
+items = [bin1, bin2, bin3, bin4, temp, rh]
+for item in items:
+    subprocess.call(["curl",url,"-u",auth,"-d",json.dumps(item),"--cacert",cert])

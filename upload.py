d877cd25e6e5acc8809252055056061e511ab6c2
Brian High <bhigh@live.com>
2015-02-04 18:27:56 -0800

    A little more readable.

--- upload.py
+++ upload.py
@@ -2,33 +2,33 @@ import subprocess
 import sys
 import json
 
-# Initialize variables
 url="https://replicant.deohs.washington.edu/api/v1/datapoints"
 cert="/etc/ssl/certs/cacert4.pem"
 auth="USERNAME:PASS"
 
-# Functions
 def prepdata(value, tags):
     "This prepares a data structure to send in JSON format"
-    data = [
-               {
-                   "name" :      sys.argv[1], 
-                   "timestamp" : sys.argv[2] + "000", 
-                   "value":      value, 
-                   "tags" :      tags
-               }
-           ]
-    return data
+    data = {
+               "name" :      sys.argv[1], 
+               "timestamp" : sys.argv[2] + "000", 
+               "value":      value, 
+               "tags" :      tags
+           }
+    return [data]
 
-# Create data structures
-bin1 = prepdata(sys.argv[3], { "Bin" : "bin1" })
-bin2 = prepdata(sys.argv[4], { "Bin" : "bin2" })
-bin3 = prepdata(sys.argv[5], { "Bin" : "bin3" })
-bin4 = prepdata(sys.argv[6], { "Bin" : "bin4" })
+def prepbin(n):
+    "This prepares data for a bin."
+    return prepdata(sys.argv[n+2], { "Bin" : "bin" + str(n) })
+
+# Prepare the bin data
+bins = []
+for n in range(0, 4):
+    bins.append(prepbin(n+1))
+
+# Prepare temperature and relative humidity data
 temp = prepdata(sys.argv[7] + "." + sys.argv[8], { "Temp" : "temp" })
 rh = prepdata(sys.argv[9] + "." + sys.argv[10], { "RH" : "rh" })
 
-# Send data in JSON format using curl
-items = [bin1, bin2, bin3, bin4, temp, rh]
+items = bins + [temp, rh]
 for item in items:
     subprocess.call(["curl",url,"-u",auth,"-d",json.dumps(item),"--cacert",cert])

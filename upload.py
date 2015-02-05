11927a3a01693274c37e40e11f8a85197f5f35c0
Brian High <bhigh@live.com>
2015-02-04 18:31:04 -0800

    Added some comments.

--- upload.py
+++ upload.py
@@ -2,10 +2,14 @@ import subprocess
 import sys
 import json
 
+# Initialize variables
+#
 url="https://replicant.deohs.washington.edu/api/v1/datapoints"
 cert="/etc/ssl/certs/cacert4.pem"
 auth="USERNAME:PASS"
 
+# Functions
+#
 def prepdata(value, tags):
     "This prepares a data structure to send in JSON format"
     data = {
@@ -21,14 +25,18 @@ def prepbin(n):
     return prepdata(sys.argv[n+2], { "Bin" : "bin" + str(n) })
 
 # Prepare the bin data
+#
 bins = []
 for n in range(0, 4):
     bins.append(prepbin(n+1))
 
 # Prepare temperature and relative humidity data
+#
 temp = prepdata(sys.argv[7] + "." + sys.argv[8], { "Temp" : "temp" })
 rh = prepdata(sys.argv[9] + "." + sys.argv[10], { "RH" : "rh" })
 
+# Send the data with curl
+#
 items = bins + [temp, rh]
 for item in items:
     subprocess.call(["curl",url,"-u",auth,"-d",json.dumps(item),"--cacert",cert])
